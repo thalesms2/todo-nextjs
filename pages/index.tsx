a682004369a92d66a58ec12358c81d6adfc6d8c8
Thales Minoru Sato <thalesms2@outlook.com>
2022-06-04 05:32:01 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ const Home = ({ todos }: PostProps) => {
       <div>
         <form className="flex justify-center mt-10">
           <div className="bg-gray-50 p-8 rounded-lg">
-            <h1 className="text-center mb-4">Write Todo List</h1>
+            <h1 className="text-center mb-4">Todo List</h1>
             <div className="flex space-x-2 p-2 bg-white rounded-md">
               <input
                 value={description}
